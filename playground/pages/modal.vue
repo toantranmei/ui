d515cd220ad581a2012f3890333838ddac693324
Pink Cheetah <nvhong021199@gmail.com>
2024-04-06 02:52:00 +0700

    feat(components): add atom component modal

---
+++ playground/pages/modal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const isOpen = ref(false)
+</script>
+
+<template>
+  <div>
+    <mei-button
+      label="Open"
+      @click="isOpen = true"
+    />
+
+    <mei-modal
+      v-model="isOpen"
+      fullscreen
+    >
+      aasas
+    </mei-modal>
+  </div>
+</template>
+
