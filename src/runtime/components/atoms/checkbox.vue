4ee8d8e2bfa0b6d1c9fa61691ad5bc9fb17dfcc6
dungdv100298 <dungdv100298@gmail.com>
2024-04-11 22:45:00 +0700

    feat(components): make Chip atom component ui
    META DATA: https://github.com/toantranmei/mei-nuxt3-ui/issues/16

--- src/runtime/components/atoms/checkbox.vue
+++ src/runtime/components/atoms/checkbox.vue
@@ -1,3 +1,61 @@
 <template>
-  <input type="radio">
+  <component
+    :is="as"
+    :class="containerClass"
+    v-bind="attrs"
+  >
+    <slot />
+  </component>
 </template>
+
+<script lang="ts">
+import { computed, toRef, defineComponent } from "vue";
+import type { PropType } from "vue";
+import { twMerge, twJoin } from "tailwind-merge";
+import { useMeiUI } from "../../composables/use-mei-ui";
+import { mergeConfig } from "../../utils";
+import type { Strategy } from "../../types";
+import appConfig from "#build/app.config.mjs";
+import { container } from "#mei-ui/ui-configs";
+
+const config = mergeConfig<typeof container>(
+  appConfig.meiUI.strategy as Strategy,
+  appConfig.meiUI.container,
+  container
+);
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    as: {
+      type: String,
+      default: "div",
+    },
+    class: {
+      type: [String, Object, Array] as PropType<any>,
+      default: () => "",
+    },
+    ui: {
+      type: Object as PropType<
+        Partial<typeof config> & { strategy?: Strategy }
+      >,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const { ui, attrs } = useMeiUI("container", toRef(props, "ui"), config);
+
+    const containerClass = computed(() => {
+      return twMerge(
+        twJoin(ui.value.base, ui.value.padding, ui.value.constrained),
+        props.class
+      );
+    });
+
+    return {
+      attrs,
+      containerClass,
+    };
+  },
+});
+</script>
