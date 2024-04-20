709e43cca4e0887485ad25e4274a741f3b7c2d28
toantran.mei <toantran.mei@gmail.com>
2024-04-21 02:44:49 +0700

    feat(components): add skeleton atom component
    META DATA: @toantranmei

---
+++ playground/pages/skeleton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center space-x-4">
+    <MeiSkeleton
+      class="h-12 w-12"
+      :ui="{ rounded: 'rounded-full' }"
+    />
+    <div class="space-y-2">
+      <MeiSkeleton class="h-4 w-[250px]" />
+      <MeiSkeleton class="h-4 w-[200px]" />
+    </div>
+  </div>
+</template>
--- src/runtime/components/atoms/skeleton.vue
+++ src/runtime/components/atoms/skeleton.vue
@@ -1,3 +1,57 @@
 <template>
-  <div class="skeleton" />
+  <div
+    :class="skeletonClass"
+    v-bind="attrs"
+  />
 </template>
+
+<script lang="ts">
+import { computed, toRef, defineComponent } from "vue";
+import type { PropType } from "vue";
+import { twMerge, twJoin } from "tailwind-merge";
+import { useMeiUI } from "../../composables/use-mei-ui";
+import { mergeConfig } from "../../utils";
+import type { Strategy } from "../../types";
+// @ts-expect-error
+import appConfig from "#build/app.config";
+import { skeleton } from "#mei-ui/ui-configs";
+
+const config = mergeConfig<typeof skeleton>(
+  appConfig.meiUI.strategy,
+  appConfig.meiUI.skeleton,
+  skeleton
+);
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    class: {
+      type: [String, Object, Array] as PropType<any>,
+      default: () => "",
+    },
+    ui: {
+      type: Object as PropType<
+        Partial<typeof config> & { strategy?: Strategy }
+      >,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const { ui, attrs } = useMeiUI("skeleton", toRef(props, "ui"), config);
+
+    const skeletonClass = computed(() => {
+      return twMerge(
+        twJoin(ui.value.base, ui.value.background, ui.value.rounded),
+        props.class
+      );
+    });
+
+    return {
+      // eslint-disable-next-line vue/no-dupe-keys
+      ui,
+      attrs,
+      skeletonClass,
+    };
+  },
+});
+</script>
