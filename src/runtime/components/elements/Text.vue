fbb85c4f5f605bb068a1923460df632d1b381b5a
toantran.mei <toantran.mei@gmail.com>
2024-06-15 00:19:00 +0700

    feat: add box and text component

---
+++ src/runtime/components/elements/Text.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    as: {
+      type: String,
+      default: 'p'
+    }
+  }
+})
+</script>
+
+<template>
+  <component
+    :is="as"
+    v-bind="$attrs"
+  >
+    <slot />
+  </component>
+</template>
---
+++ src/runtime/components/layout/Box.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    as: {
+      type: String,
+      default: 'div'
+    }
+  }
+})
+</script>
+
+<template>
+  <component
+    :is="as"
+    v-bind="$attrs"
+  >
+    <slot />
+  </component>
+</template>
